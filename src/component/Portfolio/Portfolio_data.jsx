e8715278225e401e6f0c7cd63da23337e6266060
Julian Dominguez <JulianDom@outlook.com.ar>
2023-04-06 15:35:05 -0300

    change image routes to hyperlinks

--- src/component/Portfolio/Portfolio_data.jsx
+++ src/component/Portfolio/Portfolio_data.jsx
@@ -5,7 +5,7 @@ const Portfolio_data = [
     category: "Website",
     totalLike: "600",
     title: "Responsive Website",
-    image: "./image/portfolio01.png",
+    image: "https://i.ibb.co/J36JkBk/portfolio01.png",
     paragraph:"Website with responsive design using html css plus javascript to add some functionality.",
     link:"https://juliandom.github.io/Color.website/"
   },
@@ -14,7 +14,7 @@ const Portfolio_data = [
     category: "Web Application",
     totalLike: "750",
     title: "Web application with call to an api",
-    image: "./image/portfolio02.png",
+    image: "https://i.ibb.co/RvQjVwr/portfolio02.png",
     paragraph:"Web application that tells the weather of the main cities of the world by calling an API",
     link:"https://juliandom.github.io/Weather/"
   },
@@ -23,7 +23,7 @@ const Portfolio_data = [
     category: "React",
     totalLike: "630",
     title: "Tenzies Game made in react",
-    image: "./image/portfolio03.png",
+    image: "https://i.ibb.co/3mGYz8p/portfolio03.png",
     paragraph: "dice game using the main properties of the react library",
     link: "https://tenzies-app-eight.vercel.app/",
   },
